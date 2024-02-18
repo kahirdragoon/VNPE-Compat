5e4ec44c9ad1c45a04c4281f46ee0ccbf11e9d47
KahirDragoon <markus.deeken@gmail.com>
2024-02-18 15:13:46 +0100

    Adding mod files

---
+++ Source/Harmony/Building_HemogenExtractor_Tick .cs
@@ -0,0 +1,30 @@
+﻿using PipeSystem;
+using RimWorld;
+using Verse;
+
+namespace VNPE_Compat
+{
+    public static class Building_HemogenExtractor_Tick
+    {
+        public static void Postfix(Building __instance)
+        {
+            if (__instance.IsHashIntervalTick(250))
+            {
+                var compResource = __instance.GetComp<CompResource>();
+                var compRefuelable = __instance.GetComp<CompRefuelable>();
+
+                if (compRefuelable != null && compResource != null && compResource.PipeNet is PipeNet net)
+                {
+                    var stored = net.Stored;
+                    while (compRefuelable.GetFuelCountToFullyRefuel() > 6 && stored > 0)
+                    {
+                        net.DrawAmongStorage(1, net.storages);
+
+                        stored--;
+                        compRefuelable.Refuel(6);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/Init.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using System;
+using Verse;
+
+namespace VNPE_Compat
+{
+    [StaticConstructorOnStartup]
+    public class Init
+    {
+        static Init()
+        {
+            Harmony harmony = new Harmony("VNPE-Compat");
+            
+            Type bioreactor = AccessTools.TypeByName("BioReactor.Building_BioReactor");
+            if (bioreactor != null)
+            {
+                var bioreactorTick = bioreactor.GetMethod("Tick");
+                var postfix = typeof(Building_BioReactor_Tick).GetMethod("Postfix");
+                harmony.Patch(bioreactorTick, postfix: new HarmonyMethod(postfix));
+            }
+
+            Type hemogenExtractor = AccessTools.TypeByName("HemogenExtractor.Building_HemogenExtractor");
+            if (hemogenExtractor != null)
+            {
+                var hemogenExtractorTick = hemogenExtractor.GetMethod("Tick");
+                var postfix = typeof(Building_HemogenExtractor_Tick).GetMethod("Postfix");
+                harmony.Patch(hemogenExtractorTick, postfix: new HarmonyMethod(postfix));
+            }
+        }
+    }
+}
